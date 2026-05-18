96ad0cfbbd3d4652006210d594e46c021066e08f
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-05-18 02:54:36 -0300

    Agregue una plantilla del error404

--- src/components/pages/Error404.tsx
+++ src/components/pages/Error404.tsx
@@ -1,9 +1,12 @@
+import Swal from "sweetalert2";
 const Error404 = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "No pudimos completar la acción que intentaste.",
+    footer: `<Link to={'/'}>Volver al Inicio</Link> `,
+  });
+  return <div></div>;
 };
 
 export default Error404;
