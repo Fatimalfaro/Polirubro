5b7aeeb30d2807f9cbecd84aeb04e6bd2711f6bf
Fátima Alfaro <fatialfaro99@gmail.com>
2026-05-19 16:19:51 -0300

    Agregue un titulo y un texto

--- src/components/pages/Carrito.tsx
+++ src/components/pages/Carrito.tsx
@@ -1,9 +1,29 @@
+import {FiShoppingCart} from "react-icons/fi";
+
+
 const Carrito = () => {
-    return (
-        <div>
-            
+  return (
+    <section className="min-h-screen bg-black text-white px-4 py-10">
+      <div className="max-w-7xl mx-auto">
+        
+        {/* TITULO */}
+        <div className="mb-10">
+          <div className="flex items-center gap-3 mb-2">
+            <FiShoppingCart className="text-4xl text-green-500" />
+
+            <h1 className="text-4xl font-bold">
+              TU <span className="text-green-500">CARRITO</span>
+            </h1>
+          </div>
+
+          <p className="text-zinc-400">
+            Revisá los productos que agregaste y finalizá tu compra.
+          </p>
         </div>
-    );
+
+      </div>
+    </section>
+  );
 };
 
 export default Carrito;
